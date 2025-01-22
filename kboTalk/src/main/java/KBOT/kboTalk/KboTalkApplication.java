d1dc13046afe0517c16f6148de5270d011f68690
seunghyun711 <seunghyun711@gmail.com>
2025-01-23 01:58:59 +0900

    init

---
+++ kboTalk/src/main/java/KBOT/kboTalk/KboTalkApplication.java
@@ -0,0 +1,13 @@
+package KBOT.kboTalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KboTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KboTalkApplication.class, args);
+	}
+
+}
---
+++ kboTalk/src/test/java/KBOT/kboTalk/KboTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package KBOT.kboTalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KboTalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
