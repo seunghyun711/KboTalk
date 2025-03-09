2891fc47ea0f22176f45d6e7304056500a20a63c
seunghyun711 <seunghyun711@gmail.com>
2025-03-10 00:43:01 +0900

    [feat] : 로그인 관련 DTO 클래스 추가

---
+++ kboTalk/src/main/java/KBOT/kboTalk/web/member/LoginDto.java
@@ -0,0 +1,39 @@
+package KBOT.kboTalk.web.member;
+
+import KBOT.kboTalk.domain.member.MemberType;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+public class LoginDto { // 로그인 관련 dto
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    @Setter
+    public static class JoinDto { // 회원가입 dto
+        @NotBlank
+        private String userId; // 회원 아이디
+
+        @NotBlank
+        private String password; // 비밀 번호
+
+        @NotBlank
+        private String checkPassword; // 비밀번호 검증을 위한 필드
+
+        @NotBlank
+        private String nickname; // 닉네임
+
+        @NotBlank
+        private String team; // 응원 팀
+
+        @NotBlank
+        private MemberType memberType; // 회원 유형
+
+        private String profileImage; // 프로필 이미지
+    }
+
+
+}
