3d429dda295f26e4734b45dace3c9d971bf71220
seunghyun711 <seunghyun711@gmail.com>
2025-03-19 19:29:06 +0900

    [refactor] : LoginDto 구조 수정

--- kboTalk/src/main/java/KBOT/kboTalk/web/member/LoginDto.java
+++ kboTalk/src/main/java/KBOT/kboTalk/web/member/LoginDto.java
@@ -8,27 +8,29 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class LoginDto { // 로그인 관련 dto
 
-    @AllArgsConstructor
-    @NoArgsConstructor
-    @Getter
-    @Setter
-    public static class JoinDto { // 회원가입 dto
-        @NotBlank
-        private String userId; // 회원 아이디
+    @NotBlank
+    private String userId; // 회원 아이디
 
-        @NotBlank
-        private String password; // 비밀 번호
+    @NotBlank
+    private String password; // 비밀 번호
 
-        @NotBlank
-        private String checkPassword; // 비밀번호 검증을 위한 필드
+    @NotBlank
+    private String checkPassword; // 비밀번호 검증을 위한 필드
 
-        @NotBlank
-        private String nickname; // 닉네임
+    @NotBlank
+    private String nickname; // 닉네임
 
-        private String profileImage; // 프로필 이미지
+    private String profileImage; // 프로필 이미지
+
+    // 비밀번호 검증 메서드
+    public boolean isPasswordMatch() {
+        return (password != null && checkPassword != null && password.equals(checkPassword));
     }
-
-
 }
+
