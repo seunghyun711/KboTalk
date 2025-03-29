7d1f92c624df4c870e383d502eb5cb40f2558b1f
seunghyun711 <seunghyun711@gmail.com>
2025-03-29 17:25:28 +0900

    [feat] : 로그인 인증 인터셉터 구현

---
+++ kboTalk/src/main/java/KBOT/kboTalk/WebConfig.java
@@ -0,0 +1,26 @@
+package KBOT.kboTalk;
+
+import KBOT.kboTalk.web.interceptor.LoginCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                // 인증 인터셉터 제외 경로
+                .excludePathPatterns("/", "/members/join", "/members/login", "/members/logout",
+                        "/api/members/**",
+                        "/js/**",
+                        "/css/**", "/*.ico", "/error",
+                        "/notice"
+                );
+
+
+    }
+}
