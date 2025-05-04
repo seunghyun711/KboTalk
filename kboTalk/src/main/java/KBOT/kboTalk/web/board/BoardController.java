1644f69bdf65c565cc7946dd763ba763a4624d08
seunghyun711 <seunghyun711@gmail.com>
2025-05-04 17:38:55 +0900

    [feat] : 게시판 작성자 정보(memberId)추가

--- kboTalk/src/main/java/KBOT/kboTalk/web/board/BoardController.java
+++ kboTalk/src/main/java/KBOT/kboTalk/web/board/BoardController.java
@@ -4,15 +4,16 @@ import KBOT.kboTalk.domain.board.Board;
 import KBOT.kboTalk.domain.board.BoardCategory;
 import KBOT.kboTalk.domain.board.BoardCategoryRepository;
 import KBOT.kboTalk.domain.board.BoardService;
+import KBOT.kboTalk.domain.member.Member;
+import KBOT.kboTalk.domain.member.MemberRepository;
+import KBOT.kboTalk.domain.member.SessionMember;
+import KBOT.kboTalk.web.SessionConst;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -24,6 +25,7 @@ public class BoardController {
     private final BoardService boardService;
     private final BoardMapper boardMapper;
     private final BoardCategoryRepository boardCategoryRepository;
+    private final MemberRepository memberRepository;
 
     // 게시글 작성 페이지 조회
     @GetMapping("/new")
@@ -33,15 +35,23 @@ public class BoardController {
 
     // 게시글 작성
     @PostMapping("/new")
-    public String createBoard(@Validated @ModelAttribute BoardCreateDto dto, BindingResult bindingResult) {
+    public String createBoard(@Validated @ModelAttribute BoardCreateDto dto, BindingResult bindingResult,
+                              @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) SessionMember loginMember) {
+
         if (bindingResult.hasErrors()) {
             log.info("create board error : {}", bindingResult.getAllErrors());
             return "board/createForm";
         }
 
+        if (loginMember == null) {
+            return "redirect:/members/loginForm"; // 인증되지 않은 사용자는 로그인 페이지로 리다이렉트
+        }
+
+        Member member = memberRepository.findById(loginMember.getMemberId()).orElseThrow();
+
         BoardCategory category = boardCategoryRepository.findById(dto.getCategoryId())
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 카테고리입니다."));
-        Board board = boardMapper.boardCreateDtoToBoard(dto, category);
+        Board board = boardMapper.boardCreateDtoToBoard(dto, category, member);
 
         boardService.createBoard(board);
 
--- kboTalk/src/main/java/KBOT/kboTalk/web/board/BoardMapper.java
+++ kboTalk/src/main/java/KBOT/kboTalk/web/board/BoardMapper.java
@@ -2,16 +2,18 @@ package KBOT.kboTalk.web.board;
 
 import KBOT.kboTalk.domain.board.Board;
 import KBOT.kboTalk.domain.board.BoardCategory;
+import KBOT.kboTalk.domain.member.Member;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BoardMapper {
     // BoardCreateDto -> Board
-    public Board boardCreateDtoToBoard(BoardCreateDto dto, BoardCategory category) {
+    public Board boardCreateDtoToBoard(BoardCreateDto dto, BoardCategory category, Member member) {
         return Board.builder()
                 .title(dto.getTitle())
                 .content(dto.getContent())
                 .category(category)
+                .member(member)
                 .build();
     }
 
