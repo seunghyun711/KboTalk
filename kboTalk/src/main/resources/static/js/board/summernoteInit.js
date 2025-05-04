5c1c242bd4ea842b6169f5a663c1cb8e117b905c
seunghyun711 <seunghyun711@gmail.com>
2025-05-04 11:45:04 +0900

    [feat] : summernote 에디터 설정

---
+++ kboTalk/src/main/resources/static/js/board/summernoteInit.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const editor = document.querySelector('#summernote');
+    if (!editor) return;
+
+    $('#summernote').summernote({
+        placeholder: '본문 내용을 입력하세요...',
+        tabsize: 2,
+        height: 300,
+        toolbar: [
+            ['style', ['style']],
+            ['font', ['bold', 'underline', 'clear']],
+            ['color', ['color']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['insert', ['link', 'picture', 'video']],
+            ['view', ['fullscreen', 'codeview', 'help']]
+        ],
+        callbacks: {
+            onImageUpload: function (files) {
+                uploadImage(files[0], function (imageUrl) {
+                    $('#summernote').summernote('insertImage', imageUrl, function ($image) { // 서버에서 리턴한 이미지 url을 summernote 에디터에 삽입
+                        $image.attr('alt', '첨부 이미지');
+                    });
+                });
+            }
+        }
+    });
+});
