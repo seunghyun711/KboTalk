ac101d223a0a18ecf3587450ebbd6bdfe6b86532
seunghyun711 <seunghyun711@gmail.com>
2025-05-23 23:29:52 +0900

    [feat] : BaseEntity 필드에 @Column 추가

--- kboTalk/src/main/java/KBOT/kboTalk/domain/BaseEntity.java
+++ kboTalk/src/main/java/KBOT/kboTalk/domain/BaseEntity.java
@@ -1,5 +1,6 @@
 package KBOT.kboTalk.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
@@ -15,9 +16,11 @@ import java.time.LocalDateTime;
 public abstract class BaseEntity {
 
     @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
+    @Column
     private LocalDateTime modifiedAt;
 
 }
